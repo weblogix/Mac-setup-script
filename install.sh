009f80fdf2f46933b3b23f4caf9e272fbbf099b4
Randy <randy@weblogix.ca>
2017-10-11 23:38:36 +0900

    added composer

--- install.sh
+++ install.sh
@@ -6,7 +6,27 @@ if [ "$EUID" -ne 0 ]
 fi
 
 
+export COLOR_NC='\e[0m' # No Color
+export COLOR_WHITE='\e[1;37m'
+export COLOR_BLACK='\e[0;30m'
+export COLOR_BLUE='\e[0;34m'
+export COLOR_LIGHT_BLUE='\e[1;34m'
+export COLOR_GREEN='\e[0;32m'
+export COLOR_LIGHT_GREEN='\e[1;32m'
+export COLOR_CYAN='\e[0;36m'
+export COLOR_LIGHT_CYAN='\e[1;36m'
+export COLOR_RED='\e[0;31m'
+export COLOR_LIGHT_RED='\e[1;31m'
+export COLOR_PURPLE='\e[0;35m'
+export COLOR_LIGHT_PURPLE='\e[1;35m'
+export COLOR_BROWN='\e[0;33m'
+export COLOR_YELLOW='\e[1;33m'
+export COLOR_GRAY='\e[0;30m'
+export COLOR_LIGHT_GRAY='\e[0;37m'
+
+
 # System tweaks
+echo "${COLOR_RED} Installling Mac OS Tweaks"
 
 # Show hidden files
 defaults write com.apple.Finder AppleShowAllFiles true
@@ -97,6 +117,9 @@ killall Finder
 
 
 # Tools
+# Install Xcode Command Line Tools (For macOS High Sierra 10.13)
+
+softwareupdate -i 'Command Line Tools (macOS High Sierra versio`n 10.13) for Xcode-9.0' --verbose
 
 
 # Check for Homebrew, install if we don't have it
