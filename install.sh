ad9b51e9b7bce40ea14ba8141382a1214746ad17
Randy Lowe <randy@Randy's.lan>
2018-12-22 17:19:46 +0900

    fixed patch for brew packages

--- install.sh
+++ install.sh
@@ -4,23 +4,26 @@
 #   then echo "Please run as root.  Some commands require root priviledges."
 #   exit
 # fi
+cd scripts
+# Mac OS tweaks
+./macosx.sh
 
 # Apps + Packages (Also installs Xcode Command Line Tools)
-scripts/homebrew.sh
+./homebrew.sh
 
 # Mac OS SSH Keychain fix
 # https://github.com/jirsbek/SSH-keys-in-macOS-Sierra-keychain
-sudo cp ./scripts/ssh.add.a.plist /Library/LaunchAgents/
+sudo cp ./ssh.add.a.plist /Library/LaunchAgents/
 
 # Local Environment
 #scripts/xcode.sh
-scripts/zsh.sh
-scripts/git.sh
+./zsh.sh
+./git.sh
 
 # Web Dev
 
 #scripts/composer.sh
 #scripts/dev_dns.sh
 
-
+cd ..
 source ~/.zshrc
