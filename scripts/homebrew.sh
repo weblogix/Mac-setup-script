a21ba70d833750a126e59241d5a06026a3d3bc67
Randy Lowe <randy@weblogix.ca>
2021-03-14 12:51:07 +0900

    Updated brew cask command

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Homebrew requires Xcode license to be agreed
-sudo xcodebuild -license accept
-
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 brew install $(cat brew_packages.txt)
-brew cask install $(cat brew_casks.txt)
+brew install --cask \
+google-chrome \
+visual-studio-code \
+onyx \
+appcleaner \
